b367d1a0fdaf683f1e54c3c8f139764d6fe6d347
Ankit kamboj <ankitkamboj@Ankits-MacBook-Pro.local>
2023-03-23 23:35:33 +0530

    Updates

---
+++ Contact.901b27cf.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequirec64e;e.register("248A0",(function(o,d){Object.defineProperty(o.exports,"__esModule",{value:!0}),o.exports.default=void 0;var i=e("ayMG0");var l=()=>(0,i.jsx)("div",{children:"This is contact page"});o.exports.default=l}));
+//# sourceMappingURL=Contact.901b27cf.js.map
